4435ff147221ef3a28755ba4a70e7625a0e0a1a6
gs <gs@xyz.de>
2025-06-12 22:19:24 +0200

    upload version 0.2 of navalnorth_branch_gs_ideas

--- bubble/navalnorth_branch_gs_ideas/flutter_bootstrap.js
+++ bubble/navalnorth_branch_gs_ideas/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2573058390"
+    serviceWorkerVersion: "74708741"
   }
 });
