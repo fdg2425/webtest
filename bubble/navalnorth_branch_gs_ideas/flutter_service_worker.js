5f236353fba68f26a4b717bef3024bf367553033
gs <gs@xyz.de>
2025-07-05 23:10:46 +0200

    upload version 0.5 of navalnorth_branch_gs_ideas

--- bubble/navalnorth_branch_gs_ideas/flutter_service_worker.js
+++ bubble/navalnorth_branch_gs_ideas/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3315be7b1706f516df07d1698e9cff15
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "dc815d027d67cad2905281de3faed979",
+"flutter_bootstrap.js": "cc67c7059c27296e75e2457984b28d58",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c68da7f14f4295e686b34b9852665477",
 "/": "c68da7f14f4295e686b34b9852665477",
-"main.dart.js": "2b3c1bde1f00fafc2a1dc8177b9fa701",
+"main.dart.js": "731c3949289d43d084a4e22387dac05f",
 "manifest.json": "2487fcbc57dba29b1f3371a43e2d7e79",
 "version.json": "21506dbde129f7d91fb9ee02a260dd2e"};
 // The application shell files that are downloaded before a service worker can
